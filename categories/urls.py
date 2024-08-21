a0393ebc4baecf8a5ffd0a4432e95e8a13a1ee98
abdullah956 <abdullaharshed956@gmail.com>
2024-08-21 18:45:38 +0500

    added page for category type

--- categories/urls.py
+++ categories/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('products/', views.products_view, name='products'),
     path('product/<int:id>/', views.product_detail_view, name='product_detail'),
+    path('products/<int:category_id>/', views.category_products_view, name='category_products'),
+    
+
+
 ]
-
-
+    
--- categories/views.py
+++ categories/views.py
@@ -10,3 +10,8 @@ def products_view(request):
 def product_detail_view(request, id):
     product = get_object_or_404(Product, id=id)
     return render(request, 'categories/product_detail.html', {'product': product})
+
+def category_products_view(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    products = Product.objects.filter(category=category)
+    return render(request, 'categories/category_products.html', {'category': category, 'products': products})
