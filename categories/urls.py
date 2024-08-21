dedc5c24ce3e43ee60341885a64cd32b34dac3d9
abdullah956 <abdullaharshed956@gmail.com>
2024-08-21 18:26:14 +0500

    product detail

--- categories/urls.py
+++ categories/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.products_view, name='products'),
+    path('product/<int:id>/', views.product_detail_view, name='product_detail'),
 ]
 
 
--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from categories.models import Category, Product
 
@@ -6,3 +6,7 @@ from categories.models import Category, Product
 def products_view(request):
     products = Product.objects.all()
     return render(request, 'categories/shop.html', {'products': products})
+
+def product_detail_view(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'categories/product_detail.html', {'product': product})
