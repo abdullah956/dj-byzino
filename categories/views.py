ce38a4d65391cb80044af18514f7a882ec99ec0a
abdullah956 <abdullaharshed956@gmail.com>
2024-08-22 20:44:38 +0500

    reviews done , next dashboard

--- categories/views.py
+++ categories/views.py
@@ -9,19 +9,24 @@ def products_view(request):
     )
     return render(request, 'categories/shop.html', {'products': products})
 
-
 def product_detail_view(request, id):
     product = get_object_or_404(Product, id=id)
     reviews = Review.objects.filter(product=product)
+    average_rating = reviews.aggregate(Avg('stars'))['stars__avg']
+
     context = {
         'product': product,
         'reviews': reviews,
+        'average_rating': average_rating,
     }
     return render(request, 'categories/product_detail.html', context)
 
+
 def category_products_view(request, category_id):
     category = get_object_or_404(Category, id=category_id)
-    products = Product.objects.filter(category=category)
+    products = Product.objects.filter(category=category).annotate(
+        average_rating=Avg('review__stars')
+    )
     return render(request, 'categories/category_products.html', {'category': category, 'products': products})
 
 
